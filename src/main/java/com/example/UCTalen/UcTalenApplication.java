35c73c20c064fd5cd211555fd0b1598df518d5a2
congnghia2005 <congnghiatran1@gmail.com>
2026-05-28 15:21:47 +0700

    push code

---
+++ src/main/java/com/example/UCTalen/UcTalenApplication.java
@@ -0,0 +1,13 @@
+package com.example.UCTalen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcTalenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcTalenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UCTalen/UcTalenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UCTalen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcTalenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
